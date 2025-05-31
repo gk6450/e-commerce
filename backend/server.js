7d09ba2e22b2844fbcfefe3907c3e494ba6b3324
Goutham Kanagarasu <gouthamgk2016@gmail.com>
2025-06-01 01:18:27 +0530

    Deleted certificate file and frontend url added in cors backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      // Allow requests with no origin (like Postman) or allowed frontend
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Optional: only if you're sending cookies or auth headers from frontend
+}));
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
